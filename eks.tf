2af220879de36c76162a63d9026f99090b84d455
krishna9160 <119475690+krishna9160@users.noreply.github.com>
2026-05-18 03:16:21 -0400

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,27 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = "simple-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 1
+    }
+  }
+}
