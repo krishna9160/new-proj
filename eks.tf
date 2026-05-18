56f8636d65f6adce885efdf5544efdde8b6c1ec8
krishna9160 <119475690+krishna9160@users.noreply.github.com>
2026-05-18 03:42:48 -0400

    Update EKS node group size parameters

--- eks.tf
+++ eks.tf
@@ -22,9 +22,9 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 2
+      desired_size   = 2
+      min_size       = 2
+      max_size       = 3
     }
   }
 }
