bac1e5ec0483ce4a9c7a9ba95a3bfd9dd667e3d1
krishna9160 <119475690+krishna9160@users.noreply.github.com>
2026-05-19 16:40:43 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+########################################
+# STEP 1 - USE DEFAULT VPC
+########################################
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -9,35 +17,102 @@ data "aws_subnets" "default" {
   }
 }
 
+########################################
+# STEP 2 - IAM ROLE FOR CLUSTER
+########################################
 
+resource "aws_iam_role" "cluster_role" {
+  name = "eks-cluster-role-demo"
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.21.0"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
 
-  cluster_name    = "simple-eks"
-  cluster_version = "1.29"
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
 
-  vpc_id     = data.aws_vpc.default.id
-  subnet_ids = data.aws_subnets.default.ids
+########################################
+# STEP 3 - CREATE EKS CLUSTER
+########################################
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = false
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+resource "aws_eks_cluster" "cluster" {
+  name     = "simple-eks-cluster"
+  role_arn = aws_iam_role.cluster_role.arn
 
-  create_kms_key = false
-  cluster_encryption_config = {}
-
-  manage_aws_auth_configmap = false
-
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.small"]
-      ami_type       = "AL2_x86_64"
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
-    }
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
+}
+
+########################################
+# STEP 4 - IAM ROLE FOR NODE GROUP
+########################################
+
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role-demo"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+########################################
+# STEP 5 - NODE GROUP (WORKERS)
+########################################
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "simple-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node,
+    aws_iam_role_policy_attachment.cni,
+    aws_iam_role_policy_attachment.ecr
+  ]
 }
